5cb8495faa672a2657303a5fd40d1b95a3f8d592
rakesh <rak1994kumar@gmail.com>
2016-07-20 13:57:44 -0700

    db changed

--- database/db.js
+++ database/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1/mitManagement',function(error){
+mongoose.connect('mongodb://127.0.0.1/crewlink',function(error){
          if(error) console.log("Error in Database Connection",+error);
          else{
              console.log("Database Connected Succcessfully");

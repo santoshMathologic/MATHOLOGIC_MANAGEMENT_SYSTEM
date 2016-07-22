bb29b51b3f02a433705c659185f90df5846aa163
rakesh <rak1994kumar@gmail.com>
2016-07-21 21:47:42 -0700

    drivingSection and sample page added

---
+++ public/ng/directives/dashboard/drivingSection/drivingSection.js
@@ -0,0 +1,14 @@
+angular.module('matApp').directive('drivingSection', ['$compile', function ($compiler) {
+    return {
+        restrict: 'E',
+        templateUrl: 'ng/directives/dashboard/drivingSection/drivingSection.tmpl.html',
+        controller: function ($scope, $state, $window, $location, $http, $resource) {
+
+            
+
+
+
+        }
+
+    }
+}]);
